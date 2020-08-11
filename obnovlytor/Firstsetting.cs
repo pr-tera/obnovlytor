d7aa2a801126ad9c96ae6a48b6f2259ec30ec295
Антон <anton-terechow@mail.ru>
2020-08-11 10:12:44 +0300

    Enter

---
+++ obnovlytor/Firstsetting.cs
@@ -0,0 +1,329 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows;
+using System.Xml;
+
+namespace obnovlytor
+{
+    class Firstsetting : XML
+    {
+        public static bool Start()
+        {
+            if (Install.GetAgent() == true)
+            {
+                if (GenParametrsXML() == true)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+    class XML
+    {
+        internal static bool GenParametrsXML()
+        {
+            XmlDocument XmlParam = new XmlDocument();
+            XmlDeclaration XmlDec = XmlParam.CreateXmlDeclaration("1.0", null, null);
+            XmlParam.AppendChild(XmlDec);
+            //Корень <Options>
+            XmlElement xOptions = XmlParam.CreateElement("Options");
+            XmlParam.AppendChild(xOptions);
+            //PublicKey
+            Data.PublicKey = DateTime.Now + Crypto.PrivatKey + DateTime.Now + Data.Path;
+            XmlElement xPublicKey = XmlParam.CreateElement("PublicKey");
+            xPublicKey.InnerText = Crypto.Encrypt(Data.PublicKey, Crypto.PrivatKey);
+            xOptions.AppendChild(xPublicKey);
+            /*
+             */
+            ConsoleKeyInfo key;
+            var rule = @"[aA-zZ0-9!@#№$%&?*]";
+            var buffer = "";
+            //Console.CancelKeyPress += new ConsoleCancelEventHandler(GetBuff);
+            /*
+             */
+            //login
+            Console.Write("Логин:");
+            while (true)
+            {
+                key = Console.ReadKey(true);
+                if (key.Key == ConsoleKey.Enter)
+                {
+                    if (!string.IsNullOrEmpty(buffer))
+                    {
+                        Data.Login = buffer.ToString();
+                    }
+                    buffer = "";
+                    break;
+                }
+                if (key.Key == ConsoleKey.Backspace)
+                {
+                    if (buffer.Length != 0)
+                    {
+                        buffer = buffer.Remove(buffer.Length - 1);
+                        Console.Clear();
+                        Console.Write("Логин:" + buffer);
+                    }
+                }
+                if (key.Key == ConsoleKey.V && key.Modifiers == ConsoleModifiers.Control)
+                {
+                    Data.Login = Clipboard.GetText();
+                    Console.Write(Clipboard.GetText());
+                }
+                if (System.Text.RegularExpressions.Regex.IsMatch(key.KeyChar.ToString(), rule))
+                {
+                    buffer += key.KeyChar;
+                    Console.Write(key.KeyChar);
+                }
+            }
+            XmlElement xLogin = XmlParam.CreateElement("Login");
+            xLogin.InnerText = Crypto.Encrypt(Data.Login, Data.PublicKey);
+            xOptions.AppendChild(xLogin);
+            //Password
+            Console.Write("\nПароль:");
+            while (true)
+            {
+                key = Console.ReadKey(true);
+                if (key.Key == ConsoleKey.Enter)
+                {
+                    if (!string.IsNullOrEmpty(buffer))
+                    {
+                        Data.Password = buffer.ToString();
+                    }
+                    buffer = "";
+                    break;
+                }
+                if (key.Key == ConsoleKey.Backspace)
+                {
+                    if (buffer.Length != 0)
+                    {
+                        buffer = buffer.Remove(buffer.Length - 1);
+                        Console.Clear();
+                        Console.Write("Пароль:" + buffer);
+                    }
+                }
+                if (key.Key == ConsoleKey.V && key.Modifiers == ConsoleModifiers.Control)
+                {
+                    Data.Password = Clipboard.GetText();
+                    Console.Write(Clipboard.GetText());
+                }
+                if (System.Text.RegularExpressions.Regex.IsMatch(key.KeyChar.ToString(), rule))
+                {
+                    buffer += key.KeyChar;
+                    Console.Write(key.KeyChar);
+                }
+            }
+            //Data.Password = Console.ReadLine();
+            XmlElement xPassword = XmlParam.CreateElement("Pwd");
+            xPassword.InnerText = Crypto.Encrypt(Data.Password, Data.PublicKey);
+            xOptions.AppendChild(xPassword);
+            //Console.Write("Количество дней хранения:");
+            //Data.Days = Console.ReadLine();
+            Data.Days = "3";
+            XmlElement xDays = XmlParam.CreateElement("Days");
+            xDays.InnerText = Data.Days;
+            xOptions.AppendChild(xDays);
+            SetPath();
+            if (!string.IsNullOrEmpty(Data.Login) && !string.IsNullOrEmpty(Data.Password) && !string.IsNullOrEmpty(Data.PublicKey))
+            {
+                /*вызвать функцию для активации
+                 * активироватьАгента()
+                 * {бла-бла}
+                 */
+                try
+                {
+                    XmlParam.Save(Data.Path + @"/Parametrs.xml");
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    Data.Log += $"{DateTime.Now} {e} \n";
+                    return false;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Введены не все данные\n");
+                GenParametrsXML();
+                return false;
+            }
+        }
+        private static void GetBuff(object sender, ConsoleCancelEventArgs args)
+        {
+            Data.Buffer = Clipboard.GetText();
+        }
+        static void SetPath()
+        {
+            Reqistry.GetKey();
+            if (File.Exists(Version.ImagePathAgent + @"\backup.path"))
+            {
+                File.Delete(Version.ImagePathAgent + @"\backup.path");
+            }
+            using (FileStream fstream = new FileStream(Version.ImagePathAgent + @"\backup.path", FileMode.OpenOrCreate))
+            {
+                using (StreamWriter sw = new StreamWriter(fstream, Encoding.Default))
+                {
+                    sw.Write(Request.LocalPath);
+                }
+            }
+        }
+    }
+    class Install
+    {
+        internal static bool GetAgent()
+        {
+            if (DownloadAgetn() == true)
+            {
+                if (InstallAgent() == true)
+                {
+                    Directory.Delete(Data.Path + @"\backupagentTemp", true);
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+        private static string Temp()
+        {
+            string backupagentTemp = Data.Path + @"\backupagentTemp";
+            if (!Directory.Exists(backupagentTemp))
+            {
+                try
+                {
+                    Directory.CreateDirectory(backupagentTemp);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    Data.Log += $"{DateTime.Now} {e} \n";
+                }
+            }
+            //else
+            //{
+            //    Directory.Delete(backupagentTemp, true);
+            //    Temp();
+            //}
+            return backupagentTemp;
+        }
+        private static bool DownloadAgetn()
+        {
+            string uri = "http://agentupdprod.hb.bizmrg.com/installAgent.exe";
+            try
+            {
+                using (var client = new WebClient())
+                {
+                    client.DownloadFile(uri, $"{Temp()}\\installagent.exe");
+                }
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"При загрузке произошла ошибка:\n{e}");
+                Data.Log += $"{DateTime.Now} {e} \n";
+                return false;
+            }
+        }
+        private static bool InstallAgent()
+        {
+            try
+            {
+                var process = new Process();
+                process.StartInfo.FileName = $"{Temp()}\\installagent.exe";
+                process.Start();
+                process.WaitForExit();
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Data.Log += $"{DateTime.Now} {e} \n";
+                return false;
+            }
+        }
+    }
+    class Crypto : Encryptc
+    {
+        internal static string PrivatKey { get; } = "dHJGddhsbf&3gASt38c!!!xbKASdhgd*82JBSFhjks8ASbdjaod8**wbbka(*@jk";
+        internal static string Encrypt(string str, string keyCrypt)
+        {
+            return Convert.ToBase64String(encrypt(Encoding.UTF8.GetBytes(str), keyCrypt));
+        }
+        internal static string Decrypt(string str, string keyCrypt)
+        {
+            string Result;
+            try
+            {
+                CryptoStream Cs = InternalDecrypt(Convert.FromBase64String(str), keyCrypt);
+                StreamReader Sr = new StreamReader(Cs);
+                Result = Sr.ReadToEnd();
+                Cs.Close();
+                Cs.Dispose();
+                Sr.Close();
+                Sr.Dispose();
+            }
+            catch (CryptographicException)
+            {
+                return null;
+            }
+            return Result;
+        }
+    }
+    class Encryptc : Decrypt
+    {
+        protected static byte[] encrypt(byte[] key, string value)
+        {
+            SymmetricAlgorithm Sa = Rijndael.Create();
+            ICryptoTransform Ct = Sa.CreateEncryptor((new PasswordDeriveBytes(value, null)).GetBytes(16), new byte[16]);
+            MemoryStream Ms = new MemoryStream();
+            CryptoStream Cs = new CryptoStream(Ms, Ct, CryptoStreamMode.Write);
+            Cs.Write(key, 0, key.Length);
+            Cs.FlushFinalBlock();
+            byte[] Result = Ms.ToArray();
+            Ms.Close();
+            Ms.Dispose();
+            Cs.Close();
+            Cs.Dispose();
+            Ct.Dispose();
+            return Result;
+        }
+    }
+    class Decrypt
+    {
+        protected static CryptoStream InternalDecrypt(byte[] key, string value)
+        {
+            SymmetricAlgorithm sa = Rijndael.Create();
+            ICryptoTransform ct = sa.CreateDecryptor((new PasswordDeriveBytes(value, null)).GetBytes(16), new byte[16]);
+            MemoryStream ms = new MemoryStream(key);
+            return new CryptoStream(ms, ct, CryptoStreamMode.Read);
+        }
+    }
+    struct Data
+    {
+        internal static string Path { get; set; } = Directory.GetCurrentDirectory();
+        internal static string PathPort { get; } = @"C:\1C\1cbackup-agent\metadata\port.properties";
+        internal static string PublicKey { get; set; }
+        internal static string Login { get; set; }
+        internal static string Password { get; set; }
+        internal static string Log { get; set; }
+        internal static string Days { get; set; }
+        internal static string Buffer { get; set; }
+    }
+}
---
+++ obnovlytor/Program.cs
@@ -0,0 +1,388 @@
+﻿using Microsoft.Win32;
+using obnovlytor.Properties;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace obnovlytor
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            if (args.Length == 1 && args[0] == "Update")
+            {
+                //try
+                //{
+                //    FTP.GetFolder();
+                //}
+                //catch (Exception ex)
+                //{
+                //    Console.WriteLine(ex);
+                //    Console.ReadLine();
+                //}
+                //try
+                //{
+                //    if (Function.Check() == false)
+                //    {
+                //        //Function.Update();
+                //        Function.GetVersion();
+                //    }
+                //    else
+                //    {
+                //        Function.GetVersion();
+                //    }
+                //}
+                //catch (Exception ex)
+                //{
+                //    Console.WriteLine(ex);
+                //    Console.ReadLine();
+                //}
+
+                Function.Start();
+            }
+            //else if (args.Length == 1 && args[0] == "test")
+            //{
+            //    Function.LocalInstall();
+            //}
+            else
+            {
+                if (Reqistry.CheckAgent() == true)
+                {
+                    if (File.Exists(Data.Path + @"\Parametrs.xml"))
+                    {
+                        File.Delete(Data.Path + @"\Parametrs.xml");
+                        XML.GenParametrsXML();
+                        FTP.GetFolder();
+                        if (Function.Check() == false)
+                        {
+                            Function.Update();
+                        }
+                    }
+                    else
+                    {
+
+                        try
+                        {
+                            XML.GenParametrsXML();
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex);
+                            Console.ReadLine();
+                        }
+                        try
+                        {
+                            FTP.GetFolder();
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex);
+                            Console.ReadLine();
+                        }
+                        if (Function.Check() == false)
+                        {
+                            Function.Update();
+                        }
+                    }
+                }
+                else if (Reqistry.CheckAgent() == false)
+                {
+                    if (Firstsetting.Start() == true)
+                    {
+                        FTP.GetFolder();
+                        Function.Update();
+                        Console.WriteLine("Установка произведена успешно");
+                        Log.Write();
+                        Console.ReadLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine("В ходе установки возникли ошибки.");
+                        Log.Write();
+                        Console.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+    class Function
+    {
+        public static bool Check()
+        {
+            bool Exists = false;
+            foreach (var file in Request.FileList)
+            { 
+                string[] tokens = file.Split(new[] { ' ' }, 9, StringSplitOptions.RemoveEmptyEntries);
+                string filename = tokens[8];
+                string filepath = Request.LocalPath + filename;
+                if (!File.Exists(filepath))
+                {
+                    Exists = false;
+                }
+                else
+                {
+                    Exists = true;
+                }
+            }
+            return Exists;
+        }
+        public static void GetVersion()
+        {
+            if (File.Exists(Version.FilePath))
+            {
+                using (StreamReader sr = new StreamReader(Version.FilePath, Encoding.Default))
+                {
+                    Version.VersionLocal = Convert.ToInt32(sr.ReadLine());
+                }
+                FTP.Download(Version.FTPPath);
+                if (Version.VersionLocal != 000)
+                {
+                    using (StreamReader sr = new StreamReader(Version.FilePath, Encoding.Default))
+                    {
+                        Version.VersionCurent = Convert.ToInt32(sr.ReadLine());
+                    }
+                    if (Version.VersionCurent > Version.VersionLocal)
+                    {
+                        Update();
+                    }
+                    else if (Version.VersionCurent == Version.VersionLocal)
+                    {
+
+                    }
+                }
+                else
+                {
+                    Update();
+                }
+            }
+            else
+            {
+                Update();
+            }
+        }
+        public static void Update()
+        {
+            foreach (var file in Request.FileList)
+            {
+                string[] tokens = file.Split(new[] { ' ' }, 9, StringSplitOptions.RemoveEmptyEntries);
+                string filename = tokens[8];
+                string filepath =  Request.LocalPath + filename;
+                if (File.Exists(filepath))
+                {
+                    File.Delete(filepath);
+                    FTP.Download(filename);
+                }
+                else
+                {
+                    FTP.Download(filename);
+                }
+            }
+        }
+        internal static void Start()
+        {
+            try
+            {
+                var process = new Process();
+                process.StartInfo.FileName = $"{Data.Path}\\1cbacupcloud3.5.exe";
+                process.StartInfo.Arguments = "Upload";
+                process.Start();
+                process.WaitForExit();
+            }
+            catch (Exception e)
+            {
+                Data.Log += e;
+            }
+        }
+        internal static void LocalInstall()
+        {
+            try
+            {
+                File.WriteAllBytes($"{Data.Path}\\1cbacupcloud4.5.exe", Resources._1cbacupcloud4_5);
+                File.WriteAllBytes($"{Data.Path}\\ver.ver", Resources.ver);
+                File.WriteAllText($"{Data.Path}\\backup.bat", Resources.backup);
+                File.WriteAllText($"{Data.Path}\\clean.bat", Resources.clean);
+            }
+            catch (Exception e)
+            {
+                Data.Log += e;
+            }
+        }
+    }
+    class FTP
+    {
+        public static void Download(string File)
+        {
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(getURI(File));
+            request.Method = WebRequestMethods.Ftp.DownloadFile;
+            request.Credentials = new NetworkCredential(Request.Login, Request.Password);
+            request.EnableSsl = false;
+            FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+
+            Stream responseStream = response.GetResponseStream();
+
+            FileStream fs = new FileStream(File, FileMode.Create);
+
+            byte[] buffer = new byte[64];
+            int size = 0;
+            while ((size = responseStream.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                fs.Write(buffer, 0, size);
+            }
+            fs.Close();
+            response.Close();
+        }
+        public static void GetFolder()
+        {
+            FtpWebRequest listRequest = (FtpWebRequest)WebRequest.Create(getURI(null));
+            listRequest.Method = WebRequestMethods.Ftp.ListDirectoryDetails;
+            listRequest.Credentials = new NetworkCredential(Request.Login, Request.Password);
+            using (FtpWebResponse listResponse = (FtpWebResponse)listRequest.GetResponse())
+            using (Stream listStream = listResponse.GetResponseStream())
+            using (StreamReader listReader = new StreamReader(listStream))
+            {
+                while (!listReader.EndOfStream)
+                {
+                    Request.FileList.Add(listReader.ReadLine());
+                }
+            }
+
+        }
+        static string getURI(string File)
+        {
+            string uri = string.Empty;
+            if (string.IsNullOrEmpty(File))
+            {
+                uri = Request.Head + Request.ServerDDNS + Request.Port + Request.Folder;
+                if (CheckFTP(uri) == false)
+                {
+                    uri = Request.Head + Request.ServerRezerv + Request.Port + Request.Folder;
+                    if (CheckFTP(uri) == false)
+                    {
+                        uri = Request.Head + Request.ServetRezerv2 + Request.Port + Request.Folder;
+                    }
+                }
+            }
+            else
+            {
+                uri = Request.Head + Request.ServerDDNS + Request.Port + Request.Folder + File;
+                if (CheckFTP(uri) == false)
+                {
+                    uri = Request.Head + Request.ServerDDNS + Request.Port + Request.Folder + File;
+                    if (CheckFTP(uri) == false)
+                    {
+                        uri = Request.Head + Request.ServerDDNS + Request.Port + Request.Folder + File;
+                        if (CheckFTP(uri) == false)
+                        {
+                            Function.LocalInstall();
+                        }
+                    }
+                }
+            }
+            return uri;
+        }
+        static bool CheckFTP(string uri)
+        {
+            bool check = false;
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(uri);
+            request.Method = WebRequestMethods.Ftp.ListDirectoryDetails;
+            request.Credentials = new NetworkCredential(Request.Login, Request.Password);
+            FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+            switch (response.StatusCode)
+            {
+                case FtpStatusCode.AccountNeeded:
+                    check = false;
+                    break;
+                case FtpStatusCode.OpeningData:
+                    check = true;
+                    break;
+                case FtpStatusCode.CommandOK:
+                    check = true;
+                    break;
+            }
+            response.Close();
+            return check;
+        }
+    }
+    class Log
+    {
+        internal static void Write()
+        {
+            if (!string.IsNullOrEmpty(Data.Log))
+            {
+                using (FileStream fstream = new FileStream($"{Data.Path}\\LogBackupAgent.txt", FileMode.OpenOrCreate))
+                {
+                    using (StreamWriter sw = new StreamWriter(fstream, System.Text.Encoding.Default))
+                    {
+                        sw.Write(Data.Log);
+                    }
+                }
+
+            }
+        }
+    }
+    class Reqistry
+    {
+        public static void GetKey()
+        {
+            try
+            {
+                RegistryKey key = Registry.LocalMachine;
+                key = key.OpenSubKey(@"SYSTEM\CurrentControlSet\Services\1CBackupAgent");
+                Version.ImagePathAgent = key.GetValue("ImagePath").ToString().Replace("agentNET.exe", "").Replace('"', ' ');
+            }
+            catch (Exception ex)
+            {
+                Data.Log += ex;
+            }
+        }
+        public static bool CheckAgent()
+        {
+            try
+            {
+                RegistryKey key = Registry.LocalMachine;
+                if (key.OpenSubKey(@"SYSTEM\CurrentControlSet\Services\1CBackupAgent") == null)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Data.Log += ex;
+                return false;
+            }
+        }
+    }
+    struct Request
+    {
+        internal static string Head { get; } = "ftp://";
+        internal static string ServerDDNS { get; } = "1eskaftp.hldns.ru";
+        internal static string ServerRezerv { get; } = "37.44.44.180";
+        internal static string ServetRezerv2 { get; } = "194.87.94.189";
+        internal static string Port { get; } = ":22526";
+        internal static string Login { get; } = "prftp";
+        internal static string Password { get; } = "NWaSTvpe7buLscifP1dJHclpbUNxf9Xn1FChQQQTCB";
+        //internal static string Login { get; } = "test1eska";
+        //internal static string Password { get; } = "thvLaQ8dIv8zTPKdc7wf63hu5nLRVBAmov3Qx1lpKGbaW";
+        internal static string Folder { get; } = "/cloud2/";
+        internal static List<string> FileList = new List<string>();
+        internal static string File { get; set; }
+        internal static string LocalPath { get; } = Directory.GetCurrentDirectory() + @"\Update.exe";
+    }
+    struct Version
+    {
+        internal static string FilePath { get; } = Directory.GetCurrentDirectory() + @"\ver.ver";
+        internal static string FTPPath { get; } = @"ver.ver";
+        internal static int VersionLocal { get; set; } = 000;
+        internal static int VersionCurent { get; set; }
+        internal static string ImagePathAgent { get; set; }
+    }
+}
